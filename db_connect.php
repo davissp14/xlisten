2f39d5a00fd22477bd9c9486fb20f5842ed0a527
Shaun Davis <davissp14@gmail.com>
2013-02-21 13:15:37 -0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,7 +4,7 @@ $mosConfig_locale_use_gettext = 0;
 // Edit your database settings
 $db_host = "mysql.davissp.com";
 $db_user = "davissp14";
-$db_pass = "ginger";
+$db_pass = "XxXx";
 $db_name = "xlisten";
     $connection = @mysql_connect($db_host,$db_user,$db_pass);
     if (!(mysql_select_db($db_name,$connection)))
